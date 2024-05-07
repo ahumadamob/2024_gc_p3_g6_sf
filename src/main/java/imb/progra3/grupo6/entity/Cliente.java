d7e6bcaff2191906c5fd437b6698a78ab261486f
Alvaro-Antonii <alvaro_sanchez13@hotmail.com>
2024-05-07 19:45:37 -0300

    Agregada la entidad Cliente.java

---
+++ src/main/java/imb/progra3/grupo6/entity/Cliente.java
@@ -0,0 +1,106 @@
+package imb.progra3.grupo6.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	 private String apellido;
+	 private String nombre;
+	 private String correoElectronico;
+	 private String direccionEnvio;
+	 private String numeroTelefono;
+	 
+	 
+	    
+	    public Cliente() {}
+	    
+	    
+	    public Cliente(String apellido, String nombre, String correoElectronico, String direccionEnvio, String numeroTelefono) {
+	        this.apellido = apellido;
+	        this.nombre = nombre;
+	        this.correoElectronico = correoElectronico;
+	        this.direccionEnvio = direccionEnvio;
+	        this.numeroTelefono = numeroTelefono;
+	    }
+	
+	    
+	    //getters y setters
+	    
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getApellido() {
+	        return apellido;
+	    }
+
+	    public void setApellido(String apellido) {
+	        this.apellido = apellido;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public String getCorreoElectronico() {
+	        return correoElectronico;
+	    }
+
+	    public void setCorreoElectronico(String correoElectronico) {
+	        this.correoElectronico = correoElectronico;
+	    }
+
+	    public String getDireccionEnvio() {
+	        return direccionEnvio;
+	    }
+
+	    public void setDireccionEnvio(String direccionEnvio) {
+	        this.direccionEnvio = direccionEnvio;
+	    }
+
+	    public String getNumeroTelefono() {
+	        return numeroTelefono;
+	    }
+
+	    public void setNumeroTelefono(String numeroTelefono) {
+	        this.numeroTelefono = numeroTelefono;
+	    }
+	    
+	    
+	    
+	    //toString
+	    
+	    @Override
+	    public String toString() {
+	        return "Cliente{" +
+	                "id=" + id +
+	                ", apellido='" + apellido + '\'' +
+	                ", nombre='" + nombre + '\'' +
+	                ", correoElectronico='" + correoElectronico + '\'' +
+	                ", direccionEnvio='" + direccionEnvio + '\'' +
+	                ", numeroTelefono='" + numeroTelefono + '\'' +
+	               
+	                '}';
+	    }
+	    
+	    
+	    
+	    
+}
+
