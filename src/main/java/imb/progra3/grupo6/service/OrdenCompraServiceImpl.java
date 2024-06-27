c0f338e64a47a24881531880dfad9642b10bfdbb
Iván Segal <ivaan.segal@gmail.com>
2024-06-27 13:35:11 -0300

    Añadida interfaz e implementación de servicios de la OrdenCompra

---
+++ src/main/java/imb/progra3/grupo6/service/OrdenCompraServiceImpl.java
@@ -0,0 +1,47 @@
+package imb.progra3.grupo6.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import imb.progra3.grupo6.entity.OrdenCompra;
+import imb.progra3.grupo6.repository.OrdenCompraRepository;
+
+@Service
+public class OrdenCompraServiceImpl implements IOrdenCompraService {
+	
+	@Autowired
+	private OrdenCompraRepository ordenCompraRepository;
+
+	@Override
+	public List<OrdenCompra> getAll() {
+		
+		return ordenCompraRepository.findAll();
+	}
+
+	@Override
+	public OrdenCompra getById(Long id) {
+		
+		return ordenCompraRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public OrdenCompra save(OrdenCompra ordenCompra) {
+		
+		return ordenCompraRepository.save(ordenCompra);
+	}
+
+	@Override
+	public void delete(Long id) {
+		ordenCompraRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public boolean exists(Long id) {
+		
+		return id == null ? false : ordenCompraRepository.existsById(id) ;
+	}
+
+}
