ba58c9e769e3d8c30f9003d864e8c9d9651f7b17
Iván Segal <ivaan.segal@gmail.com>
2024-09-27 17:46:29 -0300

    Se agregaron las validaciones a la clase Producto

--- src/main/java/imb/progra3/grupo6/entity/Producto.java
+++ src/main/java/imb/progra3/grupo6/entity/Producto.java
@@ -11,6 +11,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Producto {
@@ -19,18 +25,32 @@ public class Producto {
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 	 	
+	    @NotEmpty(message = "El nombre no puede estar vacío.")
+	    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres.")
 	 	private String nombre;
-
+	 	
+	 	@Size(max = 500, message = "La descripción no debe exceder los 500 caracteres.")
 	 	private String descripcion;
-
+	 	
+	    @NotNull(message = "El precio no puede ser nulo.")
+	    @Positive(message = "El precio debe ser un valor positivo.")
 	 	private Double precio;
 	 	
+	    @NotNull(message = "El stock no puede ser nulo.")
+	    @Positive(message = "El stock debe ser un valor positivo.")
+	    @Pattern(regexp = "\\d+", message = "El stock debe ser un número entero.")
 	 	private String stock;
 	 	
+	    @NotEmpty(message = "La categoría no puede estar vacía.")
+	    @Size(min = 3, max = 50, message = "La categoría debe tener entre 3 y 50 caracteres.")
 	 	private String categoria;
 	 	
+	 	@Size(max = 1000, message = "Los detalles técnicos no deben exceder los 1000 caracteres.")
 	 	private String detallesTecnicos;
 	 	
+	    @NotNull(message = "La URL de la imagen no puede ser nula.")
+	    @Pattern(regexp = "(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)", 
+	             message = "Debe ser una URL válida de imagen (jpg, gif, png).")
 	 	private String urlImagenProducto;
 	 	
 	   private LocalDate fechaReposicion;
