ba4779b27630acda4321678b4d4f16a123a31682
Iván Segal <ivaan.segal@gmail.com>
2024-09-29 21:05:24 -0300

    Revert "Creadas clases de manejo de Excepciones"
    This reverts commit b10fbf402752390370d8be34cbd849a98d798a78.

--- src/main/java/imb/progra3/grupo6/exception/ProductoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package imb.progra3.grupo6.exception;
-
-public class ProductoNotFoundException extends RuntimeException {
-	public ProductoNotFoundException(String message) {
-		super("No es posible realizar la petición porque: "+message);
-	}
-}
